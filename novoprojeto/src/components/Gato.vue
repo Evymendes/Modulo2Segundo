33a0867c2a173b006e8ec7364e4526dd0dd0debe
vainawebcpx <vainawebcpx@gmail.com>
2017-10-09 17:04:21 -0300

    Pwe

---
+++ novoprojeto/src/components/Gato.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<img src="https://i.pinimg.com/736x/22/a2/19/22a21941d8e5fce3c6e9e3a8a258cef4--galaxy-cat-hipster-cat.jpg">
+		<p> {{nome}}</p>
+		<p> {{data}}</p> 
+		<p>{{descricao}}</p>
+	</div>
+	
+</template>
+
+<script>
+	export default {
+		name: 'Gato',
+		props: ['nome', 'data', 'descricao' ]		
+	}	
+</script>
+
+<style>
+	div img{
+		width:30%;
+	}
+</style>
---
+++ novoprojeto/src/components/Hello.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <h1>{{ titulo }}</h1>
+    <Gato
+     nome='Pepeu'
+     data='12/12/2012'
+     descricao='Um dos filhotes que ficou conosco quando sua mãe o teve ele e o seu irmão já falecido por veneno Tete'
+     imagem='https://i.pinimg.com/736x/22/a2/19/22a21941d8e5fce3c6e9e3a8a258cef4--galaxy-cat-hipster-cat.jpg'> </Gato>
+   
+  </section>
+</template>
+
+<script>
+  import Gato from './Gato'
+
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        titulo: 'Portifólio',
+      }
+    },
+        components: {Gato}
+  }
+</script>
+
+<style>
+
+</style>
